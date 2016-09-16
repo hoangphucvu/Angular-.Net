755ca8f42ea10db967ebc12753fb6c3f647b2706
Phuc Ngo <ngohungphuc95@gmail.com>
2016-09-16 11:01:02 +0700

    Angular with route

---
+++ AngularWithRoute/AngularWithRoute/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularWithRoute")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularWithRoute")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb5a3e10-aa09-47fd-8788-409d04441e73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AngularWithRoute/AngularWithRoute/Scripts/AngularJsRouting/Routing.js
@@ -0,0 +1,43 @@
+﻿var app = angular.module('MyApp', ['ngRoute']);
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode({
+        enable: true,
+        requireBase: false
+    }).hashPrefix('!');
+
+    $routeProvider.when('/', {
+        redirectTo: function () {
+            return '/home';
+        }
+    })
+    .when('/home', {
+        templateUrl: '/Template/Home.html',
+        controller: 'HomeController'
+    })
+    .when('/about', {
+        templateUrl: '/Template/About.html',
+        controller: 'AboutController'
+    })
+    .when('/order/:id', {
+        templateUrl: '/Template/Order.html',
+        controller: 'OrderController'
+    })
+    .otherwise({   // This is when any route not matched
+        templateUrl: '/Template/Error.html',
+        controller: 'ErrorController'
+    })
+    //pretty url  // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+}]).controller('HomeController', function ($scope) {
+    $scope.Message = "This is HOME page";
+})
+.controller('AboutController', function ($scope) {
+    $scope.Message = "This is ABOUT page";
+})
+.controller('OrderController', function ($scope, $routeParams) {
+    // $routeParams used for get query string value
+    $scope.Message = "This is ORDER Page with query string id value " + $routeParams.id;
+})
+.controller('ErrorController', function ($scope) {
+    $scope.Message = "404 Not Found!";
+});
